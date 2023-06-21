514e327f617ba78c4038ca9648aa516746a19d7a
pradeepsabic <mail.pradeep.sharma@gmail.com>
2023-06-21 20:55:34 +0530

    added provider and main

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-Sabic-Dev"
+  location = "westIndia"
+
+}
---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+          version = "2.81.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  
+  
+}
+
